285e800cb6e7ec4b764db95e9a35e598c7e660d1
Muhammad Umar <muhammad@Muhammads-MacBook-Pro-4.local>
2026-02-17 00:45:26 +0500

    Umar: Added Square model extending Shape

---
+++ app/src/main/java/com/example/versif/Square.java
@@ -0,0 +1,11 @@
+package com.example.versif;
+
+public class Square extends Shape {
+    private int side;
+
+    public Square(int x, int y, int side) {
+        this.x = x;
+        this.y = y;
+        this.side = side;
+    }
+}
