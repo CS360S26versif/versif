870ee3c4fe30a830cae553292e022433c9b954cb
Atlantis004 <ayaanahmedsh04@gmail.com>
2026-02-16 22:59:18 +0500

    Ayaan: Added Triangle model extending Shape

---
+++ app/src/main/java/com/example/versif/Triangle.java
@@ -0,0 +1,13 @@
+package com.example.versif;
+
+public class Triangle extends Shape {
+    private int base;
+    private int height;
+
+    public Triangle(int x, int y, int base, int height) {
+        this.x = x;
+        this.y = y;
+        this.base = base;
+        this.height = height;
+    }
+}
